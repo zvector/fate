66b77de09ccaa46d54d874128510163846482677
Nick Fargo <nick@zvector.com>
2011-12-01 16:21:35 -0800

    add `addSubstate` test

--- test/unit/State.test.js
+++ test/unit/State.test.js
@@ -6,8 +6,24 @@ var	Deferral = Fate.Deferral,
 	State = Deferral.State;
 
 1&&
-asyncTest( "State", function () {
-	var	root = new State({
+asyncTest( "addSubstate", function () {
+	var	root = new State,
+	var a = root.addSubstate( 'a' );
+	var b = root.addSubstate( 'b' );
+	var c = b.addSubstate( 'c' );
+	var d = b.addSubstate( 'd' );
+	
+	ok( a instanceof State && Z.isFunction( a.name ) );
+	ok( b instanceof State && Z.isFunction( b.name ) );
+	ok( c instanceof State && Z.isFunction( c.name ) );
+	ok( d instanceof State && Z.isFunction( d.name ) );
+
+	start();
+});
+
+1&&
+asyncTest( "Interpreting nested substates", function () {
+	var	root = State({
 			a: null,
 			b: {
 				c: null,
